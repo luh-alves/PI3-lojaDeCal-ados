8e6757f3082d8005ed7c75ac5d9c81f3b71fdfd3
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-04-29 18:12:46 -0300

    ConnectionFactory ajustada e testada

--- src/main/java/caecae/pi3/ConnectionFactory.java
+++ src/main/java/caecae/pi3/ConnectionFactory.java
@@ -13,14 +13,17 @@ import java.sql.SQLException;
  * @author rolucon
  */
 public class ConnectionFactory {
-    private static String nameBD = "MangaStore";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static String nameBD = "lorentiBD";
     private static final String IP = "localhost:3306";
-    private static final String URL = "jdbc:mysql://"+IP+"/"+ nameBD;
+    private static final String URL = "jdbc:mysql://localhost:3306/" + nameBD;
     private static final String USER = "root";
     private static final String PW = "";
     
-    public static java.sql.Connection getConnection(){
+    public static java.sql.Connection getConnection() throws ClassNotFoundException{
+        System.out.println(URL);
         try {
+            Class.forName(DRIVER);
             return DriverManager.getConnection(URL,USER,PW);
         } catch (SQLException ex) {
             throw new RuntimeException("Erro de Conexao", ex);
