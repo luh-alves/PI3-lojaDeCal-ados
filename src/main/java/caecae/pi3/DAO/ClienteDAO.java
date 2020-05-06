074110e18e3530311505055b746124421fd33877
Felipe <felipeferreirahenriques@outlook.com>
2020-05-06 19:25:09 -0300

    Model pronta, esqueleto da DAO pronto

---
+++ src/main/java/caecae/pi3/DAO/ClienteDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caecae.pi3.DAO;
+
+import caecae.pi3.ConnectionFactory;
+import caecae.pi3.model.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Felipe
+ */
+public class ClienteDAO {
+    
+     public List<Cliente> listar() throws SQLException, ClassNotFoundException {
+         return null;
+       }
+            
+    
+    public void incluir(Cliente c) throws SQLException {
+       
+    }
+    
+    public void excluir(Cliente c) throws SQLException {
+        
+    }
+    
+    public void alterar(Cliente c) throws SQLException{
+        
+    }
+    
+}
